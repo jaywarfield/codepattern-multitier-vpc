bef0ddaf39d9356a1ec0a3cae49aceddbab923ba
jaywarfield <jay.warfield@us.ibm.com>
2020-12-17 14:11:05 -0500

    Update README

--- ansible/ansible.tf
+++ ansible/ansible.tf
@@ -1,39 +1,43 @@
-data "ibm_schematics_workspace" "vpc" {
-  workspace_id = var.workspace_id
-}
+#data "ibm_schematics_workspace" "vpc" {
+#  workspace_id = var.workspace_id
+#}
 
-data "ibm_schematics_output" "vpc" {
-  workspace_id = var.workspace_id
-  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
-}
+#data "ibm_schematics_output" "vpc" {
+#  workspace_id = var.workspace_id
+#  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
+#}
 
-data "ibm_schematics_state" "vpc" {
-  workspace_id = var.workspace_id
-  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
-}
+#data "ibm_schematics_state" "vpc" {
+#  workspace_id = var.workspace_id
+#  template_id  = "${data.ibm_schematics_workspace.vpc.template_id.0}"
+#}
 
 resource "local_file" "terraform_source_state" {
-  filename          = "${path.module}/ansible-data/schematics.tfstate"
-  sensitive_content = data.ibm_schematics_state.vpc.state_store_json
+   filename          = "${path.module}/terraform.tfstate"
+#  filename          = "${path.module}/ansible-data/schematics.tfstate"
+#  sensitive_content = data.ibm_schematics_state.vpc.state_store_json
 
 }
 
-output "app_dns_hostname" {
-  value = data.ibm_schematics_output.vpc.output_values["app_dns_hostname"]
-
-}
+#output "app_dns_hostname" {
+#  value = data.ibm_schematics_output.vpc.output_values["app_dns_hostname"]
+#}
 
 resource "null_resource" "ansible" {
   connection {
-    bastion_host = data.ibm_schematics_output.vpc.output_values["bastion_host_ip_address"]
-    host         = "0.0.0.0"
+    #bastion_host = data.ibm_schematics_output.vpc.output_values["bastion_host_ip_address"]
+    #host         = "0.0.0.0"
     #private_key = "${file("~/.ssh/ansible")}"
-    private_key = var.ssh_private_key
+    #private_key = var.ssh_private_key
+    bastion_host = module.bastion.bastionserver-zone1-fip
+    host_key = "${var.ssh-public-key}"
+    type = "ssh"
   }
 
   triggers = {
     always_run = timestamp()
   }
+
   provisioner "ansible" {
     plays {
       playbook {
@@ -52,15 +56,15 @@ resource "null_resource" "ansible" {
   depends_on = [local_file.terraform_source_state]
 }
 
-variable "ssh_private_key" {
-}
+#variable "ssh_private_key" {
+#}
 
 
 # variable "ibmcloud_api_key" {
 #   description = "IBM Cloud API key when run standalone"
 # }
 
-variable "workspace_id" {
-  description = "Id of the source Schematics Workspace for target VSIs"
-  default     = "ssh_bastion-host-0353ce37-3748-4c"
-}
+#variable "workspace_id" {
+#  description = "Id of the source Schematics Workspace for target VSIs"
+#  default     = "ssh_bastion-host-0353ce37-3748-4c"
+#}
