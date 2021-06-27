26de5108e6a5612a9f3db71c6882b620dae39e7f
jaywarfield-deveoper <jaywarfield.deveoper@gmail.com>
2021-06-26 20:34:11 -0400

    Update inventory.tf

--- inventory.tf
+++ inventory.tf
@@ -2,11 +2,12 @@
 resource "local_file" "inventory" {
 filename = "/tmp/.schematics/hosts.ini"
 content = <<EOT
-[dbtier]
-${module.backend.dbserver-zone1-ip}
-${module.backend.dbserver-zone2-ip}
 [webapptier]
 ${module.frontend.webappserver-zone1-ip} 
 ${module.frontend.webappserver-zone2-ip}
+[dbtier0]
+${module.backend.dbserver-zone1-ip}
+[dbtier1]
+${module.backend.dbserver-zone2-ip}
 EOT
 }
